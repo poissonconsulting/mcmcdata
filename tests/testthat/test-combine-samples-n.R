9e42ea9039eaf3810f63567b909e245f409ffab9
Joe Thorley <joe@poissonconsulting.ca>
2018-03-14 08:47:43 -0700

    add test combine_samples_n.list

--- tests/testthat/test-combine-samples-n.R
+++ tests/testthat/test-combine-samples-n.R
@@ -4,4 +4,6 @@ test_that("combine_samples_n", {
   mcmc_data_example <- mcmcdata::mcmc_data_example
   mcmc_data_example <- dplyr::select(mcmc_data_example, -films)
   expect_equal(mcmcr::combine_samples_n(mcmc_data_example, mcmc_data_example, mcmc_data_example, by = c("homeworld", "species")), mcmc_data_example, check.attributes = FALSE)
+  
+  expect_equal(mcmcr::combine_samples_n(list(mcmc_data_example, mcmc_data_example, mcmc_data_example), by = c("homeworld", "species")), mcmc_data_example, check.attributes = FALSE)
 })
