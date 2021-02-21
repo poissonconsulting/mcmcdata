c8ab3783d9ff6ea511f7da0f2fb89ff3a6ff9207
Joe Thorley <joe@poissonconsulting.ca>
2021-02-21 12:11:09 -0800

    - Add parameter argument to mcmc_derive and have derived and current be the defaults.

--- R/mcmc-derive.R
+++ R/mcmc-derive.R
@@ -1,14 +1,30 @@
+#' mcmc_derive mcmc_data
+#' 
+#' @param object An mcmc_data object
+#'
+#' @param expr A string of the R expression.
+#' @param values A named list of values for objects not in object$data
+#' @param parameter A string of the name of the current MCMC samples in expr.
+#' @param monitor A regular expression identifying the name of the derived MCMC samples in expr.
+#' @param monitor A string of the 
+#' @param parallel A flag specifying whether to run in parallel.
+#' @param ... An unused.
+#'
 #' @export
-mcmc_derive.mcmc_data <- function(object, expr = "prediction <- estimate", 
+mcmc_derive.mcmc_data <- function(object, expr = "derived <- current", 
                                   values = list(), 
-                                  monitor = "prediction", parallel = TRUE, ...) {
+                                  parameter = "current",
+                                  monitor = "^derived$",
+                                  parallel = TRUE, ...) {
   check_string(expr)
   check_string(monitor)
+  check_string(parameter)
+  check_flag(parallel)
   check_unused(...)
   
   data <- as.data.frame(object)
   mcmc <- as.mcmcr(object)
-  pars(mcmc) <- "estimate"
+  pars(mcmc) <- parameter
   values <- c(values, as.list(data))
   mcmc <- mcmc_derive(mcmc, expr = expr, monitor = monitor, values = values,
                       parallel = TRUE)
--- tests/testthat/test-mcmc-derive.R
+++ tests/testthat/test-mcmc-derive.R
@@ -2,7 +2,7 @@ test_that("derive.mcmc_data", {
   
   mcmc_data <- mcmcdata::mcmc_data_example
   mcmc_data$data$count <- 1:2
-  expect_warning(derived <- mcmc_derive(mcmc_data, expr = "prediction <- estimate * count"))
+  expect_warning(derived <- mcmc_derive(mcmc_data, expr = "derived <- current * count"))
   expect_is(derived, "mcmc_data")
   
   mcmc_data <- coef(mcmc_data, simplify = TRUE)
