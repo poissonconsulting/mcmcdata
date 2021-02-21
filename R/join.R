444807cffc75a7b79899023f019b79c408e208d5
Joe Thorley <joe@poissonconsulting.ca>
2021-02-21 13:49:48 -0800

    - Added `right_join()`.

--- R/join.R
+++ R/join.R
@@ -72,9 +72,37 @@ left_join.mcmc_data <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", 
   
   data <- x$data
   data$..IDX <- 1:nrow(data)
-  data <- dplyr::anti_join(data, y, by = by, copy = copy, suffix = suffix, ...)
+  data <- dplyr::left_join(data, y, by = by, copy = copy, suffix = suffix, ...)
   
   mcmc <- x$mcmc
+  mcmc <- mcmc[,,data$..IDX,drop = FALSE]
+  class(mcmc) <- "mcmcarray"
+  
+  data$..IDX <- NULL
+  mcmc_data(mcmc, data)
+}
+
+#' Left join mcmc data
+#'
+#' @inheritParams dplyr::inner_join
+#' @export
+right_join.mcmc_data <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...){
+  y <- as.data.frame(y)
+  check_missing_colnames(x$data, "..IDX")
+  check_missing_colnames(y, "..IDX")
+  
+  data <- x$data
+  data$..IDX <- 1:nrow(data)
+  ..IDXn <- nrow(data) + 1L
+  
+  data <- dplyr::right_join(data, y, by = by, copy = copy, suffix = suffix, ...)
+  data$..IDX[is.na(data$..IDX)] <- ..IDXn
+
+  mcmc <- x$mcmc
+
+  mcmc_na <- mcmc[,,1,drop = FALSE]
+  is.na(mcmc_na) <- TRUE
+  mcmc <- abind::abind(mcmc, mcmc_na, along = 3L)
   mcmc <- mcmc[,,data$..IDX,drop = FALSE]
   class(mcmc) <- "mcmcarray"
   
--- tests/testthat/test-join.R
+++ tests/testthat/test-join.R
@@ -2,10 +2,19 @@ test_that("join", {
   expect_identical(inner_join(mcmc_data_example, select(mcmc_data_example, -films),
                               by = c("homeworld", "species")), mcmc_data_example)
   expect_identical(anti_join(mcmc_data_example, select(slice(mcmc_data_example, 1), -films),
-                              by = c("homeworld", "species")), slice(mcmc_data_example,2))
+                             by = c("homeworld", "species")), slice(mcmc_data_example,2))
   expect_identical(semi_join(mcmc_data_example, mcmc_data_example,
-                              by = c("homeworld", "species")), mcmc_data_example)
-#  expect_identical(left_join(mcmc_data_example, select(slice(mcmc_data_example, 1), -films),
-#                              by = c("homeworld", "species")), mcmc_data_example)
-})
+                             by = c("homeworld", "species")), mcmc_data_example)
+  expect_identical(left_join(mcmc_data_example, select(slice(mcmc_data_example, 1), -films),
+                             by = c("homeworld", "species")), mcmc_data_example)
   
+  right <- right_join(select(slice(mcmc_data_example, 1), -films), mcmc_data_example,
+                      by = c("homeworld", "species"))
+  expect_identical(right$data, mcmc_data_example$data)
+  
+  coef_right <- coef(right, simplify = TRUE)
+  expect_identical(colnames(coef_right), 
+                   c("homeworld", "species", "films", "estimate", "lower", "upper", 
+                     "svalue"))
+  expect_equal(coef_right$estimate, c(3.718025, NA))
+})
